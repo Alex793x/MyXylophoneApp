57e87bb2cb0574c0b3c805554bb38cc60a794087
Alex793x <Alex793x@stud.kea.dk>
2023-01-05 15:49:11 +0100

    added colors, sound and UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,52 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void playSound(int num) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$num.wav'));
+  }
+
+  Expanded soundButtons(Color color, int value, String string) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            playSound(value);
+            if (kDebugMode) {
+              print('$string button was clicked');
+            }
+          },
+          child: Container(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'My Xylophone App',
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              soundButtons(Colors.red, 1, 'red'),
+              soundButtons(Colors.orange, 2, 'orange'),
+              soundButtons(Colors.yellow, 3, 'yellow'),
+              soundButtons(Colors.green, 4, 'green'),
+              soundButtons(Colors.teal, 4, 'teak'),
+              soundButtons(Colors.blue, 4, 'blue'),
+              soundButtons(Colors.purple, 4, 'purple'),
+            ],
+          ),
         ),
       ),
     );
